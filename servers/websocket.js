780562d536aa5961333d6aaf4ca7df2f1c2f69ae
caulfield27 <al1onggwp@gmail.com>
2025-07-16 15:59:36 +0500

    finish battlefield web socket

--- servers/websocket.js
+++ servers/websocket.js
@@ -44,29 +44,188 @@ ws.on("connection", (socket, _) => {
         };
         if (selectionStack.length) {
           const opponent = selectionStack.pop();
-          const roomId = uuidv4();
-          gameRooms.set(roomId, [player, opponent]);
           const oppSocket = clients.get(opponent.id);
+          if (oppSocket) {
+            const roomId = uuidv4();
+            gameRooms.set(roomId, [player, opponent]);
+
+            const turn = Math.round(Math.random());
+            socket.send(
+              JSON.stringify({
+                type: "gameFound",
+                data: {
+                  name: opponent.name,
+                  sessionId: roomId,
+                },
+              })
+            );
+            socket.send(
+              JSON.stringify({
+                type: "turn",
+                data: turn === 0 ? 0 : 1,
+              })
+            );
+            oppSocket.send(
+              JSON.stringify({
+                type: "turn",
+                data: turn === 1 ? 0 : 1,
+              })
+            );
+            oppSocket.send(
+              JSON.stringify({
+                type: "gameFound",
+                data: {
+                  name: player.name,
+                  sessionId: roomId,
+                },
+              })
+            );
+          }
+        } else {
+          selectionStack.push(player);
+        }
+
+        break;
+      case "closeRoom":
+        const { roomId } = data;
+        if (gameRooms.has(roomId)) {
+          const players = gameRooms.get(roomId);
+          for (const player of players) {
+            if (player.id !== socket.clientId) {
+              const playerSocket = clients.get(player.id);
+              if (playerSocket) {
+                playerSocket.send(
+                  JSON.stringify({
+                    type: "roomClosed",
+                    data: null,
+                  })
+                );
+              }
+            }
+          }
+          gameRooms.delete(data);
+        }
+        break;
+      case "check":
+        const { sessionId, coordinates } = data;
+        if (gameRooms.has(sessionId)) {
+          const players = gameRooms.get(sessionId);
+          for (const player of players) {
+            if (player.id !== socket.clientId) {
+              const playerSocket = clients.get(player.id);
+              if (playerSocket) {
+                playerSocket.send(
+                  JSON.stringify({
+                    type: "check",
+                    data: { coordinates },
+                  })
+                );
+              }
+            }
+          }
+        }
+        break;
+      case "status":
+        const room = data.roomId;
+        const dataCoordinates = data.coordinates;
+        const status = data.status;
+
+        const players = gameRooms.get(room);
+        if (players) {
+          for (const player of players) {
+            if (player.id !== socket.clientId) {
+              const playerSocket = clients.get(player.id);
+              if (playerSocket) {
+                const msg = {
+                  type: "status",
+                  data: {
+                    status,
+                    coordinates: dataCoordinates,
+                  },
+                };
+                if (status === "destroy") msg.data["range"] = data.range;
+                playerSocket.send(JSON.stringify(msg));
+                if (status === "lose") {
+                  gameRooms.delete(room);
+                } else {
+                  playerSocket.send(
+                    JSON.stringify({
+                      type: "turn",
+                      data: status === "hit" || status === "destroy" ? 1 : 0,
+                    })
+                  );
+                }
+              }
+            } else {
+              const playerSocket = clients.get(player.id);
+              if (status === "lose") {
+                gameRooms.delete(room);
+                playerSocket.send(
+                  JSON.stringify({
+                    type: "lose",
+                    data: null,
+                  })
+                );
+              } else {
+                playerSocket.send(
+                  JSON.stringify({
+                    type: "turn",
+                    data: status === "hit" || status === "destroy" ? 0 : 1,
+                  })
+                );
+              }
+            }
+          }
+        }
+        break;
+      case "invite":
+        const { key } = data;
+        if (gameRooms.has(key)) {
+          const players = gameRooms.get(key);
+          players.push({
+            name: "",
+            id: socket.clientId,
+          });
+          const [firstPlayer, _] = players;
+          const opponent = clients.get(firstPlayer.id);
+          const turn = Math.round(Math.random());
           socket.send(
             JSON.stringify({
               type: "gameFound",
               data: {
-                name: opponent.name,
-                sessionId: roomId,
+                name: firstPlayer.name,
+                sessionId: key,
               },
             })
           );
-          oppSocket.send(
+          socket.send(
+            JSON.stringify({
+              type: "turn",
+              data: turn === 0 ? 0 : 1,
+            })
+          );
+          opponent.send(
+            JSON.stringify({
+              type: "turn",
+              data: turn === 1 ? 0 : 1,
+            })
+          );
+          opponent.send(
             JSON.stringify({
               type: "gameFound",
               data: {
-                name: player.name,
-                sessionId: roomId,
+                name: "",
+                sessionId: key,
               },
             })
           );
         } else {
-          selectionStack.push(player);
+          gameRooms.set(key, [
+            {
+              name: "",
+              id: socket.clientId,
+            },
+          ]);
         }
     }
   });
