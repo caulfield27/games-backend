4cb192a239a8184543b78a1cd06887b277638de8
caulfield27 <al1onggwp@gmail.com>
2025-09-02 11:19:35 +0500

    update keep alive time

--- utils/keepAlive.js
+++ utils/keepAlive.js
@@ -2,4 +2,4 @@ setInterval(() => {
   fetch("https://games-online-service.onrender.com")
     .then(() => console.info("wake up bro"))
     .catch((e) => console.error("keep alive err: ", e));
-}, 600000);
+}, 20 * 60 * 1000);
